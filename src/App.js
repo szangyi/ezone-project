7142ac781397d7d42814e73f3dcaa87d47ff2063
Alexandra Mészáros <alexandrameszaros@Alex-Macbook.local>
2021-05-15 11:03:23 +0200

    netlify again

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.scss";
-// import { Quiz } from "./form_steps/quiz.js";
-// import { Background } from "./background/background.js";
-import { FrontPage } from "./frontpage/frontpage.js";
+import { Quiz } from "./form_steps/quiz.js";
+import { Background } from "./background/background.js";
+// import { FrontPage } from "./frontpage/frontpage.js";
 
 function App() {
   return (
     <div className="App">
-      <FrontPage />
-      {/* <Quiz /> */}
-      {/* <Background /> */}
+      {/* <FrontPage /> */}
+      <Quiz />
+      <Background />
     </div>
   );
 }
